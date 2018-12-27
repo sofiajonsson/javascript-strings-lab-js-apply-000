65d2c704eabb90e385c25764cc75e376a36e93e1
Sofia Jonsson <jonssonsofia93@gmail.com>
2018-12-27 05:56:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
-"Hello," + specialGuest +"!";
+var specialGuest = "Neil deGrasse Tyson";
+//"Hello," + specialGuest +"!";
 
-var greetSpecialGuest = `"Hello" + specialGuest + "!"`;
+var greetSpecialGuest = `"Hello", + ${specialGuest} + "!"`;
 
 var topic = "space";
 
